c7f8b753116e429678183d1e8ef9f737e3ce4ca8
Jesse <jesse@waas.ca>
2017-10-17 01:27:48 -0700

    Added FileMetaCollection, misc utility functions, and a change group based diff method.

--- fsa.py
+++ fsa.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """File system audit tool
@@ -49,6 +49,8 @@ Note:
     * Ignores empty folders.
 """
 
+from __future__ import print_function
+
 __author__ = "Jesse Waas"
 __description__ = "File system audit tool"
 __copyright__ = "Copyright 2017, Jesse Waas"
@@ -62,6 +64,7 @@ import os
 import hashlib
 import json
 import argparse
+import csv
 from fnmatch import fnmatch
 from collections import OrderedDict
 
@@ -98,7 +101,10 @@ class FileMeta(object):
         hash_value: File hash value
     """
 
-    def __init__(self, file_path, hash_algorithm=HASH_FN):
+    KEYS = ["name", "path", "mode", "uid", "gid", "size", "atime", "mtime",
+            "ctime", "hash"]
+
+    def __init__(self, file_path=None, hash_algorithm=HASH_FN, from_dict=None):
         """Create file meta-data object
 
         Initialize file meta-data object. File stat information is read,
@@ -116,19 +122,26 @@ class FileMeta(object):
         Args:
             file_path: Path to file of interest
             hash_algorithm: hashlib Hash algorithm (default hashlib.sha256())
+            import_dict: Dict representation of existing FileMeta object to
+                         clone. "file_path" And "hash_algorithm" arguments
+                         ignored.
         """
-        file_stat = os.stat(file_path)
 
-        self.name = os.path.split(file_path)[1]
-        self.path = file_path
-        self.mode = "{:o}".format(file_stat.st_mode & 0o777)
-        self.uid = file_stat.st_uid
-        self.gid = file_stat.st_gid
-        self.size = file_stat.st_size
-        self.atime = file_stat.st_atime
-        self.mtime = file_stat.st_mtime
-        self.ctime = file_stat.st_ctime
-        self.hash_value = hash_file(file_path, hash_algorithm)
+        if from_dict:
+            self.from_dict(from_dict)
+        else:
+            file_stat = os.stat(file_path)
+
+            self.name = os.path.split(file_path)[1]
+            self.path = file_path
+            self.mode = "{:o}".format(file_stat.st_mode & 0o777)
+            self.uid = file_stat.st_uid
+            self.gid = file_stat.st_gid
+            self.size = file_stat.st_size
+            self.atime = file_stat.st_atime
+            self.mtime = file_stat.st_mtime
+            self.ctime = file_stat.st_ctime
+            self.hash_value = hash_file(file_path, hash_algorithm)
 
     def to_list(self):
         return [self.name,
@@ -141,6 +154,18 @@ class FileMeta(object):
                 self.mtime,
                 self.ctime,
                 self.hash_value]
+
+    def from_dict(self, import_dict):
+        self.name = import_dict["name"]
+        self.path = import_dict["path"]
+        self.mode = import_dict["mode"]
+        self.uid = import_dict["uid"]
+        self.gid = import_dict["gid"]
+        self.size = import_dict["size"]
+        self.atime = import_dict["atime"]
+        self.mtime = import_dict["mtime"]
+        self.ctime = import_dict["ctime"]
+        self.hash_value = import_dict["hash"]
 
     def to_dict(self):
         return OrderedDict([("name", self.name),
@@ -198,6 +223,244 @@ class FileMeta(object):
     def __str__(self):
         return json.dumps(self.to_dict(), indent=2)
 
+    def __getitem__(self, attr):
+        # "hash_value" known externally as "hash" TODO: Clean up.
+        if attr == "hash": attr = "hash_value"
+        return self.__dict__[attr]
+
+
+class FileMetaCollection(object):
+    """File meta-data collection object
+
+    Encapsulates a collection of FileMeta objects loaded from a previous audit.
+    Each FileMeta instance represents an audit of a single file (on a given
+    host at a specific point of time).
+
+    MetaFile attributes can be "indexed" (made into dict keys) by passing the
+    attribute name to this classes constructor. This allows for fast lookups.
+
+    Attributes:
+        index_keys: MetaFile values to index (list)
+        name: File name (informational only)
+        meta_list: Flat list of MetaFile objects
+        meta_indexed: MetaFile objects indexed by key/value
+    """
+
+    def __init__(self, index_keys, name=None, from_iterable=None,
+                 from_json_file=None):
+
+        if isinstance(index_keys, str):
+            index_keys = [index_keys]
+
+        self.name = name
+        self.index_keys = index_keys
+        self.meta_list = []
+
+        # meta_indexed = {key = INDEX_KEY,
+        #                 val = { key = META_FILE_KEY_VALUE,
+        #                         val = META_FILE}}
+        #
+        # Example: {key = "path",
+        #           val = { key = "foo.bar",
+        #                   val = MetaFile(path="foo.bar")}}
+        #
+        self.meta_indexed = OrderedDict(
+            ((k, OrderedDict()) for k in self.index_keys))
+
+        if from_iterable:
+            self.from_iterable(from_iterable)
+
+        if from_json_file:
+            self.from_json_file(from_json_file)
+
+    def add(self, meta):
+        """Add a single MetaFile to this collection.
+
+        Args:
+            meta: MetaFile
+        """
+        self.meta_list.append(meta)
+
+        for key in self.index_keys:
+            # Note that only one FileMeta object is stored per unique index
+            # key value. Non-unique keys will be overwritten.
+            self.meta_indexed[key][meta[key]] = meta
+
+    def to_csv(self, path):
+        """Save FileMeta collection to CSV file.
+
+        Args:
+            path: Output CSV file path
+        """
+        with open(path, "wb") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(FileMeta.KEYS)
+            csv_writer.writerows((x.to_dict() for x in self.meta_list))
+
+    def to_json(self, path):
+        """Save FileMeta collection to JSON file.
+
+        Args:
+            path: Output JSON file path
+        """
+        with open(path, "w") as json_file:
+            json.dump([x.to_dict() for x in self.meta_list], json_file)
+
+    def from_iterable(self, iterable):
+        """Load MetaFiles from MetaFile iterable.
+
+        Args:
+            iterable: Iterable collection of MetaFile's such as list()
+        """
+        for meta in iterable:
+            self.add(meta)
+
+    def from_json_file(self, path):
+        """Load MetaFiles from Json file.
+
+        Args:
+            path: JSON File path
+        """
+        with open(path, "r") as meta_file:
+            raw_meta_data = json.load(meta_file)
+
+            for raw_meta in raw_meta_data:
+                meta = FileMeta(from_dict=raw_meta)
+                self.add(meta)
+
+    def get_meta_list(self):
+        """Get list of all FileMeta objects in this FileMetaCollection."""
+        return self.meta_list
+
+    def get_meta_index(self, key):
+        """Get dictionary of {value:MetaFile}'s for the specified key.
+
+        Args:
+            key: MetaFile attribute key (name|path|mode|uid|gid|size|atime|
+                                         mtime|ctime|hash)
+        """
+        return self.meta_indexed[key]
+
+    def get_meta(self, key, value):
+        """Get single FileMeta object by key and value.
+
+        Example:
+            FileMetaCollection.get_meta("path", "foo.bar")
+                ==> FileMeta(path="foo.bar")
+
+        Args:
+            key: MetaFile attribute key (name|path|mode|uid|gid|size|atime|
+                                         mtime|ctime|hash)
+            value: Value of MetaFile attribute.
+        """
+        return self.meta_indexed[key].get(value, None)
+
+
+def get_key_value_superset(file_meta_collections, primary_key):
+    """Get key value superset from a list of FileMetaCollection's.
+
+    Iterates throug N input FileMetaCollection's and accumulates a superset of
+    key values related to the specified primary key.
+
+    Note that FileMetaCollection's are themselves lists of meta data
+    for different files.
+
+    Example:
+
+    file_meta_col_1 = [{"path":1..}, {"path":2..}, {"path":3..}]
+    file_meta_col_2 = [{"path":2..}, {"path":3..}, {"path":4..}]
+
+    get_key_value_superset([file_meta_col_1, file_meta_col_2], "path")
+
+    Result: [1, 2, 3, 4]
+
+    Input order maintained where possible.
+
+    Args:
+        file_meta_collections: List of FileMetaCollectionis
+        primary_key: Primary key with which to resolve value superset
+    """
+    # Using OrderedDict as 'ordered set' to collect ordered, unique list
+    # of primary key values.
+    primary_key_value_dict = OrderedDict()
+
+    # Fetch all primary key values from input collections. Each collection
+    # may contain different primary key value sets - we want a unique super
+    # -set of these.
+    for file_meta_collection in file_meta_collections:
+        key_values = file_meta_collection.get_meta_index(primary_key)
+        primary_key_value_dict.update(key_values)
+
+    # Collapse to simple list of keys. Values are now assigned to
+    # arbitrary FileMeta instances which are not useful.
+    primary_key_values = list(primary_key_value_dict.keys())
+
+    return primary_key_values
+
+
+def group_diff(interesting_keys, meta_files):
+    """Compare meta data file interesting key values.
+
+    Performs n-way diff on specified collection of MetaFiles. The specified
+    key values are compared and "diff group" integers are defined to group
+    meta_files with common key values.
+
+    Diff groups are provided both for individual keys and for the combination
+    of all keys.
+
+    Args:
+        interesting_keys: List of key strings to compare (name|path|mode|uid|
+                                                          gid|size|atime|mtime|
+                                                          ctime|hash)
+        meta_files: List of MetaFile file meta data objects for comparison.
+    """
+    result = []
+
+    # Cache key values that are found in input meta files so we can track
+    # the number of unique values found (gives us group #)
+    #
+    # single_key_value_cache = {key = INTERESTING_KEY,
+    #                           val = { key=INTERESTING_VALUE,
+    #                                   val=DIFF_GROUP_INTEGER}}
+    #
+    single_key_value_cache = dict(((k, dict()) for k in interesting_keys))
+
+    # tuple_key_value_cache = {key = TUPLE_OF_INTERESTING_GROUP_INTEGERS
+    #                          val = DIFF_SUMMARY_GROUP_INTEGER}
+    tuple_key_value_cache = dict()
+
+    for meta in meta_files:
+
+        # Use an OrderedDict() here so that groups.items() is produced in
+        # order consistant with interesting_keys. Note that this
+        #
+        # groups = {key=INTERESTING_KEY, val=DIFF_GROUP_INTEGER}
+        groups = OrderedDict()
+
+        for key in interesting_keys:
+            cache = single_key_value_cache[key]
+            meta_key_value = meta[key]
+
+            # New DIFF_GROUP_INTEGER is equal to length of cache (number of
+            # unique values found).
+            if not meta_key_value in cache:
+                cache[meta_key_value] = len(cache)
+
+            groups[key] = cache[meta_key_value]
+
+        # Convert to immutable nested tuple to be accepted as a cache dict key
+        groups_tuple = tuple(groups.items())
+
+        # New DIFF_SUMMARY_GROUP_INTEGER is equal to length of cache (number of
+        # unique values found). Unique values comprise of tuples of
+        # DIFF_GROUP_INTEGER's related to all interesting keys.
+        if groups_tuple not in tuple_key_value_cache:
+            tuple_key_value_cache[groups_tuple] = len(tuple_key_value_cache)
+
+        result.append((meta, groups, tuple_key_value_cache[groups_tuple]))
+
+    return result
+
 
 def hash_file(path, hash_algorithm=HASH_FN):
     """Return hash of specified file.
@@ -219,7 +482,7 @@ def hash_file(path, hash_algorithm=HASH_FN):
                 hash_algorithm.update(block)
             else:
                 return hash_algorithm.hexdigest()
-                
+
 
 def ignore_file(ignore_path, ignore_files):
     """Check if specified file path matches ignore patterns
@@ -235,7 +498,7 @@ def ignore_file(ignore_path, ignore_files):
         while ignore_path:
             ignore_path, ignore_file = os.path.split(ignore_path)
             if True in (fnmatch(ignore_file, f) for f in ignore_files):
-               return True
+                return True
 
     return False
 
@@ -270,10 +533,94 @@ def walk_path(path, recursive=False, hash_algorithm=HASH_FN, ignore_files=None):
                 break
 
 
+def cmd_walk(args):
+    """Analyse files on file system."""
+    hash_algorithm_map = {"md5": hashlib.md5(),
+                          "sha1": hashlib.sha1(),
+                          "sha224": hashlib.sha224(),
+                          "sha256": hashlib.sha256(),
+                          "sha384": hashlib.sha384(),
+                          "sha512": hashlib.sha512()}
+
+    hash_algorithm = HASH_FN
+
+    if args.algorithm in hash_algorithm_map:
+        hash_algorithm = hash_algorithm_map[args.algorithm]
+
+    multi_path = len(args.path) > 1
+
+    meta_file_collection = None
+    if bool(args.csv) | bool(args.json):
+        meta_file_collection = FileMetaCollection(["path"])
+
+    for file_path in args.path:
+        if os.path.isdir(file_path) and multi_path and not args.recursive:
+            continue
+
+        for file_meta in walk_path(file_path, recursive=args.recursive,
+                                   hash_algorithm=hash_algorithm,
+                                   ignore_files=args.ignore):
+
+            if args.string:
+                print(file_meta.to_string(fmt=args.string))
+            else:
+                print(file_meta.path)
+
+            if meta_file_collection:
+                meta_file_collection.add(file_meta)
+
+    if meta_file_collection:
+        if args.csv:
+            meta_file_collection.to_csv(args.csv)
+
+        elif args.json:
+            meta_file_collection.to_json(args.json)
+
+
+def cmd_diff(args):
+    """Diff file system based on previously captured meta-data."""
+    file_meta_collections = []
+
+    PATH_KEY = "path"
+    INTERESTING_KEYS = ["hash", "size"]
+
+    # Input analysis archives
+    for path in args.diff:
+        file_meta_collection = FileMetaCollection(PATH_KEY, name=path, from_json_file=path)
+        file_meta_collections.append(file_meta_collection)
+
+    primary_key_values = get_key_value_superset(file_meta_collections, PATH_KEY)
+
+    print()
+
+    interesting_keys_txt = "".join(("{:^10}".format(k) for k in INTERESTING_KEYS))
+    column_header_txt = "{:40}{}{:^10}".format("File @ Archive", interesting_keys_txt, "sum")
+
+    print(column_header_txt)
+
+    # For each file for which we have meta-data
+    for file_key in primary_key_values:
+
+        # Obtain meta data for different version of the file
+        meta_list = [m.get_meta(PATH_KEY, file_key) for m in file_meta_collections]
+
+        diff = group_diff(INTERESTING_KEYS, meta_list)
+
+        # For each comparison key that we are interested in
+        for archive_path, (meta, key_groups, group) in zip(args.diff, diff):
+            row_name = "{} @ {}".format(file_key, archive_path)
+            key_group_txt = "".join(("{:^10}".format(g) for g in key_groups.values()))
+
+            print("{:40}{}{:^10}".format(row_name, key_group_txt, group))
+
+        print()
+
+
 def main():
     """Command line interface for generating filesystem meta-data"""
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description=
+# pylint: disable=bad-continuation
 """File system audit tool. A command line tool for collecting filesystem
 meta-data. Iterates through input files, and outputs file hash and meta-data
 using the specified algorithm and format (to stdout).
@@ -303,8 +650,8 @@ Note:
     {hash}  - File hash value
 
     Example: --string "{path}, {mode}, {size}, {hash}" """)
-
-    parser.add_argument("path", metavar="PATH", nargs="+",
+# pylint: enable=bad-continuation
+    parser.add_argument("path", metavar="PATH", nargs="*",
                         help="File(s) or path to audit.")
     parser.add_argument("-s", "--string", metavar="FORMAT",
                         help="""Output str.format template. See [1] above.""")
@@ -315,42 +662,19 @@ Note:
                              "sha224, sha256, sha384, sha512).")
     parser.add_argument("-r", "--recursive", action="store_true",
                         help="Recursively walk directory tree.")
-    parser.add_argument("--json", action="store_true",
-                        help="Output in JSON format.")
-    parser.add_argument("--csv", action="store_true",
-                        help="Output in CSV format.")
+    parser.add_argument("--json",
+                        help="Output to JSON file.")
+    parser.add_argument("--csv",
+                        help="Output to CSV file.")
+    parser.add_argument("--diff", nargs="*",
+                        help="Diff the specified archive files.")
 
     args = parser.parse_args()
 
-    hash_algorithm_map = {"md5": hashlib.md5(),
-                          "sha1": hashlib.sha1(),
-                          "sha224": hashlib.sha224(),
-                          "sha256": hashlib.sha256(),
-                          "sha384": hashlib.sha384(),
-                          "sha512": hashlib.sha512()}
-
-    hash_algorithm = HASH_FN
-
-    if args.algorithm in hash_algorithm_map:
-        hash_algorithm = hash_algorithm_map[args.algorithm]
-
-    multi_path = len(args.path) > 1
-
-    for file_path in args.path:
-        if os.path.isdir(file_path) and multi_path and not args.recursive:
-            continue
-
-        for file_meta in walk_path(file_path, recursive=args.recursive,
-                                   hash_algorithm=hash_algorithm,
-                                   ignore_files=args.ignore):
-            if args.string:
-                print(file_meta.to_string(fmt=args.string))
-            elif args.csv:
-                print(file_meta.to_string())
-            elif args.json:
-                print(file_meta.to_json())
-            else:
-                print(file_meta)
+    if args.diff:
+        cmd_diff(args)
+    else:
+        cmd_walk(args)
 
 
 if __name__ == "__main__":
